369d37302b074fb51f36abf6b696ff48b7626ad3
raichoo <raichoo@googlemail.com>
2016-06-20 22:52:02 +0200

    set insert mode after execute in normal mode

--- config.fish
+++ config.fish
@@ -80,6 +80,6 @@ function fish_user_key_bindings
   bind -e -M default \r
   bind -M insert \n force-repaint execute
   bind -M insert \r force-repaint execute
-  bind -M default \n force-repaint execute
-  bind -M default \r force-repaint execute
+  bind -M default --sets-mode insert \n force-repaint execute
+  bind -M default --sets-mode insert \r force-repaint execute
 end
